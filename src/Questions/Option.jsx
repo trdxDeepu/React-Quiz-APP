9222d1d2921d6abf9e55a9559e24972b3774fee6
trdxlucifer <singhdepu566@gmail.com>
2023-09-09 13:12:25 +0530

    added answers

--- src/Questions/Option.jsx
+++ src/Questions/Option.jsx
@@ -2,12 +2,23 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
-export const Option = ({questions,dispatch,answer}) => {
+export const Option = ({ questions, dispatch, answer }) => {
+  const hasAnswered = answer !== null;
+
   return (
     <div className="options">
-      {questions.options.map((option,index) => (
-        <button className="btn btn-option" key={option} 
-            onClick={()=>dispatch({type:'newAnswer',payload:index})}
+      {questions.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === questions.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }  `}
+          key={option}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
         >
           {option}
         </button>
